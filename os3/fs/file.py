9d37af5834de1bd342141feeb97062eb41b4e57c
Nekmo <contacto@nekmo.com>
2016-11-07 04:16:30 +0100

    Open now uses future.

--- os3/fs/file.py
+++ os3/fs/file.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from future.builtins import open
 import os
 import sys
 
--- os3/tests/fs/tests.py
+++ os3/tests/fs/tests.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from future.builtins import str, open
 import os
 import tempfile
 import unittest
-from operator import itemgetter
 from unittest import TestCase
 
 import six
