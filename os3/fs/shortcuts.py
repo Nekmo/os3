434695bf36dabf2dcfc4eab8f5dd82a7de917519
Nekmo <contacto@nekmo.com>
2016-11-10 04:07:24 +0100

    ls() repr without brackets.

--- os3/fs/shortcuts.py
+++ os3/fs/shortcuts.py
@@ -4,11 +4,13 @@ import sys
 from os3.fs.directory import DirList
 from os3.fs.entry import get_path
 from os3.fs.utils import get_node
+from os3.utils.decorators import withrepr
 
 
+@withrepr(lambda x: DirList().print_format())
 def ls(path='', depth=None, **kwargs):
     return DirList(path).ls(depth, **kwargs)
-
+ls.filter = DirList().filter
 
 def symlink(source, link_name):
     return get_node(source).symlink(link_name)
--- os3/utils/decorators.py
+++ os3/utils/decorators.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+import functools
+
+
+class reprwrapper(object):
+    def __init__(self, repr, func):
+        self._repr = repr
+        self._func = func
+        functools.update_wrapper(self, func)
+
+    def __call__(self, *args, **kw):
+        return self._func(*args, **kw)
+
+    def __repr__(self):
+        return self._repr(self._func)
+
+
+def withrepr(reprfun):
+    """Use: @withrepr(lambda x: "<Func: %s>" % x.__name__)
+    :param reprfun:
+    :return:
+    """
+    def _wrap(func):
+        return reprwrapper(reprfun, func)
+    return _wrap
